0c5bf4ee9c9143da27a3262a604c22a4b01805a7
Ravi Desai <ravi@contactmonkey.com>
2026-03-01 12:59:38 -0500

    Basic GET handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello! You requested: %s\n", r.URL.Path)
+}
 
 func main() {
-	fmt.Println("FF")
+	fmt.Println("Started program")
+
+	http.HandleFunc("/", helloHandler)
+
+	// Start the server on port 8080
+	fmt.Println("Starting server at :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Printf("Server failed: %s\n", err)
+	}
 }
