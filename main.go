ba39bb98d9500e1f4f11e19099f04b0017c619da
Ravi Desai <ravi@contactmonkey.com>
2026-03-01 13:49:40 -0500

    EntryNew endpoint
    Lots of TODOs since a lot needs to be done. Recording things so that I
    don't forget them later.

--- main.go
+++ main.go
@@ -9,12 +9,27 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello! You requested: %s\n", r.URL.Path)
 }
 
+func entryNew(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// TODO can this be parsed straight into a struct?
+	passportNumber := r.PostFormValue("passportNumber")
+	portId := r.PostFormValue("portId")
+	// TODO Shove this into a channel. The reader of the chan should know where to write the data.
+	// Reader should also (eventually) check this against the ban-list.
+	fmt.Printf("enh. pn=%s. port=%s\n", passportNumber, portId)
+	// TODO How to signal errs back to the client? Do I want websockets? If yes, then why not do writes
+	// using websockets too?
+	w.WriteHeader(http.StatusAccepted)
+}
+
 func main() {
 	fmt.Println("Started program")
 
 	http.HandleFunc("/", helloHandler)
-
-	// Start the server on port 8080
+	http.HandleFunc("/entryNew", entryNew) // POST
 	fmt.Println("Starting server at :3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		fmt.Printf("Server failed: %s\n", err)
